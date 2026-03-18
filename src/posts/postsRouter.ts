4f41f470f4755a9229e73c6a0b84b69fb7932cdb
cyb3ik <cyb3ik88@mail.ru>
2026-03-18 16:23:42 +0300

    v 1.0.1 fixed post router

--- src/posts/postsRouter.ts
+++ src/posts/postsRouter.ts
@@ -7,12 +7,13 @@ import { deletePostById } from "./handlers/deletePostByIdHandler"
 import { idValidation } from "../validation/idValidationMiddleware"
 import { postDtoValidationMiddleware } from "./validation/postDtoValidationMiddleware"
 import { inputValidationResultMiddleware } from "../validation/inputValidationResultMiddleware"
+import { authGuardMiddleware } from "../validation/authGuardMiddlewate"
 
 export const postsRouter = Router()
 
 postsRouter
     .get("/", readAllPosts)
     .get("/:id", idValidation, inputValidationResultMiddleware, readPostById)
-    .post("/", postDtoValidationMiddleware, inputValidationResultMiddleware, createPost)
-    .put("/:id", idValidation, postDtoValidationMiddleware, inputValidationResultMiddleware, updatePostById)
-    .delete("/:id", idValidation, inputValidationResultMiddleware, deletePostById)
+    .post("/", authGuardMiddleware, postDtoValidationMiddleware, inputValidationResultMiddleware, createPost)
+    .put("/:id", authGuardMiddleware, idValidation, postDtoValidationMiddleware, inputValidationResultMiddleware, updatePostById)
+    .delete("/:id", authGuardMiddleware, idValidation, inputValidationResultMiddleware, deletePostById)
